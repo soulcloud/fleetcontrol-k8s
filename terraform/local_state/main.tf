a719a1476089f302a0d7f549dd0b0d1aeff04b57
soulcloud <solomon.anamm@gmail.com>
2023-06-23 14:13:15 +0100

    created acl resource  in s3

--- terraform/local_state/main.tf
+++ terraform/local_state/main.tf
@@ -112,6 +112,32 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "kops_sse_config" 
 }
 
 
+resource "aws_s3_bucket_ownership_controls" "kops_bucket_ownership" {
+  bucket = aws_s3_bucket.kops.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "kops_bucket_public_access" {
+  bucket = aws_s3_bucket.kops.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "kops_bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.kops_bucket_ownership,
+    aws_s3_bucket_public_access_block.kops_bucket_public_access,
+  ]
+
+  bucket = aws_s3_bucket.kops.id
+  acl    = "public-read"
+}
 
 
 
