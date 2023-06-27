8884467883f6b3a67226165be899f63bf486b89a
soulcloud <solomon.anamm@gmail.com>
2023-06-27 09:20:08 +0100

    added unzip dependency and switched to githyb releases

--- terraform/local_state/install-kubectl-and-kops.sh
+++ terraform/local_state/install-kubectl-and-kops.sh
@@ -3,5 +3,16 @@
 # Install kubectl
 sudo snap install kubectl --classic
 
-# Install kops
-sudo snap install kops
+# Install kops using official download instructions
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo mv kops /usr/local/bin/kops
+
+# Install unzip
+sudo apt-get update
+sudo apt-get install -y unzip
+
+# Install AWS CLI using official download instructions
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
